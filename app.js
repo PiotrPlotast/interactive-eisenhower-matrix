1d30eca73fc83ca5dcc38924e1afdfd1a3d205d4
Pietrell <piotrplotast2006@gmail.com>
2024-01-03 21:16:12 +0100

    starting from start js code

--- app.js
+++ app.js
@@ -1,253 +1,61 @@
 // select all matrix lists
-import {
-  matrixImportantUrgentList,
-  matrixImportantNotUrgentList,
-  matrixNotImportantUrgentList,
-  matrixNotImportantNotUrgentList,
-  addToImportantUrgentList,
-  addToImportantNotUrgentList,
-  addToNotImportantUrgentList,
-  addToNotImportantNotUrgentList,
-} from "./matrixUtils.js";
-// end of selections
+const matrixImportantUrgentList = document.querySelector(
+  ".matrix__quadrant--important-urgent__list"
+);
+const matrixImportantNotUrgentList = document.querySelector(
+  ".matrix__quadrant--important-not-urgent__list"
+);
+const matrixNotImportantUrgentList = document.querySelector(
+  ".matrix__quadrant--not-important-urgent__list"
+);
+const matrixNotImportantNotUrgentList = document.querySelector(
+  ".matrix__quadrant--not-important-not-urgent__list"
+);
 
-// create matrix lists arrays to store items
-const matrixImportantUrgent = [];
-const matrixImportantNotUrgent = [];
-const matrixNotImportantUrgent = [];
-const matrixNotImportantNotUrgent = [];
-// end of creating matrix lists arrays to store items
+// select matrix lists buttons
+const addToImportantUrgentList = document.querySelector(
+  ".matrix__quadrant--important-urgent__add"
+);
+const addToImportantNotUrgentList = document.querySelector(
+  ".matrix__quadrant--important-not-urgent__add"
+);
+const addToNotImportantUrgentList = document.querySelector(
+  ".matrix__quadrant--not-important-urgent__add"
+);
+const addToNotImportantNotUrgentList = document.querySelector(
+  ".matrix__quadrant--not-important-not-urgent__add"
+);
 
-// add items to matrix lists and arrays
-function addItemToList(inputValue, list) {
-  if (inputValue === "") {
-    throw new Error("Please enter a value");
-  } else {
-    const matrixItem = document.createElement("li");
-    matrixItem.classList.add("matrix__list__item");
-    matrixItem.innerHTML = `
-    <p class="matrix__list__item__input">${inputValue}</p>
-    <button class="matrix__list__item__delete">X</button>
-    `;
-    list.prepend(matrixItem);
-
-    switch (list) {
-      case matrixImportantUrgentList:
-        matrixImportantUrgent.push(inputValue);
-        break;
-      case matrixImportantNotUrgentList:
-        matrixImportantNotUrgent.push(inputValue);
-        break;
-      case matrixNotImportantUrgentList:
-        matrixNotImportantUrgent.push(inputValue);
-        break;
-      case matrixNotImportantNotUrgentList:
-        matrixNotImportantNotUrgent.push(inputValue);
-        break;
+let matrix = {
+  importantUrgent: [],
+  importantNotUrgent: [],
+  notImportantUrgent: [],
+  notImportantNotUrgent: [],
+  importantUrgentList: matrixImportantUrgentList,
+  importantNotUrgentList: matrixImportantNotUrgentList,
+  notImportantUrgentList: matrixNotImportantUrgentList,
+  notImportantNotUrgentList: matrixNotImportantNotUrgentList,
+  add: function (item, list) {
+    if (list === this.importantUrgentList) {
+      this.importantUrgent.push(item);
+    } else if (list === this.importantNotUrgentList) {
+      this.importantNotUrgent.push(item);
+    } else if (list === this.notImportantUrgentList) {
+      this.notImportantUrgent.push(item);
+    } else if (list === this.notImportantNotUrgentList) {
+      this.notImportantNotUrgent.push(item);
     }
-  }
-}
-// end of adding items to matrix lists and arrays
+  },
+};
 
-// add items to local storage
-function addInputValueToLocalStorage() {
-  localStorage.setItem(
-    "matrixImportantUrgentItems",
-    JSON.stringify(matrixImportantUrgent)
-  );
-
-  localStorage.setItem(
-    "matrixImportantNotUrgentItems",
-    JSON.stringify(matrixImportantNotUrgent)
-  );
-
-  localStorage.setItem(
-    "matrixNotImportantUrgentItems",
-    JSON.stringify(matrixNotImportantUrgent)
-  );
-
-  localStorage.setItem(
-    "matrixNotImportantNotUrgentItems",
-    JSON.stringify(matrixNotImportantNotUrgent)
-  );
-}
-// end of adding items to local storage
-
-// delete items from local storage
-function deleteItemFromLocalStorage(list, item) {
-  console.log(list);
-  console.log(item);
-  switch (list) {
-    case "matrix__quadrant--important-urgent__list":
-      console.log(list);
-      let matrixImportantUrgentItems = JSON.parse(
-        localStorage.getItem("matrixImportantUrgentItems")
-      );
-      const index1 = matrixImportantUrgentItems.indexOf(item);
-      if (index1 !== -1) {
-        matrixImportantUrgentItems.splice(index1, 1);
-
-        const arrayString = JSON.stringify(matrixImportantUrgentItems);
-
-        localStorage.setItem("matrixImportantUrgentList", arrayString);
-      }
-      break;
-
-    case "matrix__quadrant--important-not-urgent__list":
-      let matrixImportantNotUrgentItems = JSON.parse(
-        localStorage.getItem("matrixImportantNotUrgentItems")
-      );
-      const index2 = matrixImportantNotUrgentItems.indexOf(item);
-      if (index2 !== -1) {
-        matrixImportantNotUrgentItems.splice(index2, 1);
-
-        const arrayString = JSON.stringify(matrixImportantNotUrgentItems);
-
-        localStorage.setItem("matrixImportantNotUrgentList", arrayString);
-      }
-      break;
-
-    case "matrix__quadrant--not-important-urgent__list":
-      let matrixNotImportantUrgentItems = JSON.parse(
-        localStorage.getItem("matrixNotImportantUrgentItems")
-      );
-      const index3 = matrixNotImportantUrgentItems.indexOf(item);
-      if (index3 !== -1) {
-        matrixNotImportantUrgentItems.splice(index3, 1);
-
-        const arrayString = JSON.stringify(matrixNotImportantUrgentItems);
-
-        localStorage.setItem("matrixNotImportantUrgentList", arrayString);
-      }
-      break;
-
-    case "matrix__quadrant--not-important-not-urgent__list":
-      let matrixNotImportantNotUrgentItems = JSON.parse(
-        localStorage.getItem("matrixNotImportantNotUrgentItems")
-      );
-
-      const index4 = matrixNotImportantNotUrgentItems.indexOf(item);
-      if (index4 !== -1) {
-        matrixNotImportantNotUrgentItems.splice(index4, 1);
-
-        const arrayString = JSON.stringify(matrixNotImportantNotUrgentItems);
-
-        localStorage.setItem("matrixNotImportantNotUrgentList", arrayString);
-      }
-      break;
-  }
-}
-// end of delete items from local storage
-
-// delete items from matrix lists
-function deleteItem() {
-  const deleteItems = document.querySelectorAll(".matrix__list__item__delete");
-  deleteItems.forEach((deleteItem) => {
-    const list = deleteItem.parentElement.parentElement;
-    const item = deleteItem.parentElement;
-    // console.log(deleteItem.parentElement);
-    deleteItem.addEventListener("click", (event) => {
-      console.log("test");
-      deleteItemFromLocalStorage(list, item);
-      event.target.parentElement.remove();
-    });
-  });
-}
-// end of delete items from matrix lists
-
-// add items to matrix lists on enter key press
-addToImportantUrgentList.addEventListener("keydown", (event) => {
+// add items to matrix
+addToImportantUrgentList.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
-    addItemToList(addToImportantUrgentList.value, matrixImportantUrgentList);
-    addInputValueToLocalStorage();
-    addToImportantUrgentList.value = "";
-    deleteItem();
+    const item = addToImportantUrgentList.value;
+    const li = document.createElement("li");
+    li.textContent = item;
+    matrix.add(item, matrixImportantUrgentList);
+    console.log(addToImportantUrgentList.value);
+    matrixImportantUrgentList.appendChild(li);
   }
 });
-
-addToImportantNotUrgentList.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    addItemToList(
-      addToImportantNotUrgentList.value,
-      matrixImportantNotUrgentList
-    );
-    addInputValueToLocalStorage();
-    addToImportantNotUrgentList.value = "";
-    deleteItem();
-  }
-});
-
-addToNotImportantUrgentList.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    addItemToList(
-      addToNotImportantUrgentList.value,
-      matrixNotImportantUrgentList
-    );
-    addInputValueToLocalStorage();
-    addToNotImportantUrgentList.value = "";
-    deleteItem();
-  }
-});
-
-addToNotImportantNotUrgentList.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    addItemToList(
-      addToNotImportantNotUrgentList.value,
-      matrixNotImportantNotUrgentList
-    );
-    addInputValueToLocalStorage();
-    addToNotImportantNotUrgentList.value = "";
-    deleteItem();
-  }
-});
-// end of adding items to matrix lists on enter key press
-
-// add items to matrix lists from local storage on page load
-function addItemsFromLocalStorage() {
-  const matrixImportantUrgentItems = JSON.parse(
-    localStorage.getItem("matrixImportantUrgentItems")
-  );
-
-  const matrixImportantNotUrgentItems = JSON.parse(
-    localStorage.getItem("matrixImportantNotUrgentItems")
-  );
-
-  const matrixNotImportantUrgentItems = JSON.parse(
-    localStorage.getItem("matrixNotImportantUrgentItems")
-  );
-
-  const matrixNotImportantNotUrgentItems = JSON.parse(
-    localStorage.getItem("matrixNotImportantNotUrgentItems")
-  );
-
-  if (matrixImportantUrgentItems) {
-    matrixImportantUrgentItems.forEach((item) => {
-      addItemToList(item, matrixImportantUrgentList);
-    });
-    deleteItem();
-  }
-
-  if (matrixImportantNotUrgentItems) {
-    matrixImportantNotUrgentItems.forEach((item) => {
-      addItemToList(item, matrixImportantNotUrgentList);
-    });
-    deleteItem();
-  }
-
-  if (matrixNotImportantUrgentItems) {
-    matrixNotImportantUrgentItems.forEach((item) => {
-      addItemToList(item, matrixNotImportantUrgentList);
-    });
-    deleteItem();
-  }
-
-  if (matrixNotImportantNotUrgentItems) {
-    matrixNotImportantNotUrgentItems.forEach((item) => {
-      addItemToList(item, matrixNotImportantNotUrgentList);
-    });
-    deleteItem();
-  }
-}
-
-window.onload = addItemsFromLocalStorage();
-// end of adding items to matrix lists from local storage on page load
