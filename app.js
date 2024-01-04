269d78af488e7d41c7cef47f2ed40be74af2b23b
Pietrell <piotrplotast2006@gmail.com>
2024-01-04 18:32:44 +0100

    added add to matrix functionalities

--- app.js
+++ app.js
@@ -48,14 +48,107 @@ let matrix = {
   },
 };
 
-// add items to matrix
+let matrixLocalStorage = {
+  importantUrgent: [],
+  importantNotUrgent: [],
+  notImportantUrgent: [],
+  notImportantNotUrgent: [],
+  addToLocalStorage: function (item, list) {
+    if (list === matrixImportantUrgentList) {
+      this.importantUrgent.push(item);
+      localStorage.setItem(
+        "importantUrgent",
+        JSON.stringify(this.importantUrgent)
+      );
+    } else if (list === matrixImportantNotUrgentList) {
+      this.importantNotUrgent.push(item);
+      localStorage.setItem(
+        "importantNotUrgent",
+        JSON.stringify(this.importantNotUrgent)
+      );
+    } else if (list === matrixNotImportantUrgentList) {
+      this.notImportantUrgent.push(item);
+      localStorage.setItem(
+        "notImportantUrgent",
+        JSON.stringify(this.notImportantUrgent)
+      );
+    } else if (list === matrixNotImportantNotUrgentList) {
+      this.notImportantNotUrgent.push(item);
+      localStorage.setItem(
+        "notImportantNotUrgent",
+        JSON.stringify(this.notImportantNotUrgent)
+      );
+    }
+  },
+};
+
+// add items to matrices
 addToImportantUrgentList.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
+    if (addToImportantUrgentList.value === "") return;
     const item = addToImportantUrgentList.value;
     const li = document.createElement("li");
+    const deleteItem = document.createElement("button");
+    deleteItem.textContent = "X";
+    deleteItem.classList.add("delete");
     li.textContent = item;
+    li.appendChild(deleteItem);
     matrix.add(item, matrixImportantUrgentList);
-    console.log(addToImportantUrgentList.value);
     matrixImportantUrgentList.appendChild(li);
+    addToImportantUrgentList.value = "";
+    matrixLocalStorage.addToLocalStorage(item, matrixImportantUrgentList);
   }
 });
+
+addToImportantNotUrgentList.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    if (addToImportantNotUrgentList.value === "") return;
+    const item = addToImportantNotUrgentList.value;
+    const li = document.createElement("li");
+    const deleteItem = document.createElement("button");
+    deleteItem.textContent = "X";
+    deleteItem.classList.add("delete");
+    li.textContent = item;
+    li.appendChild(deleteItem);
+    matrix.add(item, matrixImportantNotUrgentList);
+    matrixImportantNotUrgentList.appendChild(li);
+    addToImportantNotUrgentList.value = "";
+    matrixLocalStorage.addToLocalStorage(item, matrixImportantNotUrgentList);
+  }
+});
+
+addToNotImportantUrgentList.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    if (addToNotImportantUrgentList.value === "") return;
+    const item = addToNotImportantUrgentList.value;
+    const li = document.createElement("li");
+    const deleteItem = document.createElement("button");
+    deleteItem.textContent = "X";
+    deleteItem.classList.add("delete");
+    li.textContent = item;
+    li.appendChild(deleteItem);
+    matrix.add(item, matrixNotImportantUrgentList);
+    matrixNotImportantUrgentList.appendChild(li);
+    addToNotImportantUrgentList.value = "";
+    matrixLocalStorage.addToLocalStorage(item, matrixNotImportantUrgentList);
+  }
+});
+
+addToNotImportantNotUrgentList.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    if (addToNotImportantNotUrgentList.value === "") return;
+    const item = addToNotImportantNotUrgentList.value;
+    const li = document.createElement("li");
+    const deleteItem = document.createElement("button");
+    deleteItem.textContent = "X";
+    deleteItem.classList.add("delete");
+    li.textContent = item;
+    li.appendChild(deleteItem);
+    matrix.add(item, matrixNotImportantNotUrgentList);
+    matrixNotImportantNotUrgentList.appendChild(li);
+    addToNotImportantNotUrgentList.value = "";
+    matrixLocalStorage.addToLocalStorage(item, matrixNotImportantNotUrgentList);
+  }
+});
+
+const deleteItem = document.querySelectorAll(".delete");
